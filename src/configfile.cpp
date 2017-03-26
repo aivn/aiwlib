df0e351e6fa1701b6ec5f72d59f679649ac448f6
Anton V. Ivanov <aivanov@keldysh.ru>
2017-03-26 23:50:04 +0300

    Модули configfile и objconf собираются и протестированы.

--- src/configfile.cpp
+++ src/configfile.cpp
@@ -14,19 +14,28 @@ void aiw::scanf_obj(std::istream &S, bool &X){
 	for(int i=0; i<14; i++) if(value==Y[i]){ X = true; return; }
 	for(int i=0; i<14; i++) if(value==N[i]){ X = false; return; }
 	WRAISE("incorrect value for convert to bool, Y|y|YES|Yes|yes|ON|On|on|TRUE|True|true|V|v|1" 
-			   " or N|n|NO|No|no|OFF|Off|off|FALSE|False|false|X|x|0 expected", value);
+		   " or N|n|NO|No|no|OFF|Off|off|FALSE|False|false|X|x|0 expected", value);
+}
+//------------------------------------------------------------------------------
+void aiw::scanf_obj(std::istream &S, std::string &X){
+	std::string X0; std::getline(S, X0);
+	int a = 0, b = X0.size()-1;
+	while(a<=b and (X0[a]==' ' or X0[a]=='\t' or X0[a]=='\r')) a++;
+	while(a<=b and (X0[b]==' ' or X0[b]=='\t' or X0[b]=='\r')) b--;
+	X = X0.substr(a, b);
 }
 //------------------------------------------------------------------------------
 void aiw::ConfigFile::load(std::istream &&fin){
 	do {
 		std::string line;
 		std::getline(fin, line);
-		std::stringstream buf(line); std::string key, val;
-		std::getline(buf, key, '='); std::getline(buf, val, '#');
+		std::stringstream buf(line); std::string key0, key, val;
+		std::getline(buf, key0, '='); std::getline(buf, val, '#');
+		std::stringstream(key0)>>key;
 		if(key.size() and key[0]!='#') table[key] = val;
 	} while(not fin.eof());
 }
 void aiw::ConfigFile::dump(std::ostream &&S) const {
-	for(auto I=table.begin(); I!=table.end(); ++I) S<<I.first()<<'='<<I.second()<<'\n';
+	for(auto I=table.begin(); I!=table.end(); ++I) S<<"'"<<I->first<<"'='"<<I->second<<"'\n";
 }
 //------------------------------------------------------------------------------
