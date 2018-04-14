3d8ae5c76b763a555bc6ed8be56fa79b56a7aeb0
Anton V. Ivanov <aiv.racs@gmail.com>
2018-04-14 16:22:58 +0300

    Переключение на AbstratViewer из guthub Добавлен пример с уравнением Фоккера-Планка Доработана документация

--- demo/FPE/model.hpp
+++ demo/FPE/model.hpp
@@ -8,9 +8,11 @@ class Model{
 public:
     double a, b, A, Omega, gamma, T, t, h; // параметры расчета
 
-	Vec<6> av;  // моменты x, v, xx, vv, xv, W 
-	inline Vec<6> d_av(const Vec<2> &p) const { // вклад в моменты от одной траектории
-		return p|(p&p)|(p[0]*p[1])|((.5*a + .25*b*p[0]*p[0])*p[0]*p[0] + .5*p[1]*p[1]);
+	Vec<6> av;  // моменты x, v, xx, vv, xv, W
+	// вклад в моменты от одной траектории
+	inline Vec<6> d_av(const Vec<2> &p) const { 
+		return p|(p&p)|(p[0]*p[1])|
+			((.5*a + .25*b*p[0]*p[0])*p[0]*p[0] + .5*p[1]*p[1]);
 	}
     Mesh<float, 2> f; // функция распределения
 	
--- demo/FPE/run2.py
+++ demo/FPE/run2.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # импортируем необходимые модули
+from math import *
 from aiwlib.vec import *       # вектора
 from aiwlib.iostream import *  # работа с файлами
 from aiwlib.MeshF2 import *    # сетка Mesh<float,2>
@@ -10,7 +11,7 @@ from model import *            # модель
 # создаем класс модели и задаем параметры
 M = Model()
 M.a, M.b = -1, 1
-M.A, M.Omega = 0., 0.
+M.A, M.Omega = 0.1, 1.
 M.gamma, M.T = 0.1, 0.2
 M.h = 0.1
 
@@ -19,8 +20,8 @@ M.f.init(ind(100,100), vec(-2.,-1.), vec(2.,1.))
 M.f.bounds = 0x55 # режим обработки границ функции распределения
 
 # создаем файлы для записи результатов
-fout = File('f1.msh', 'w')
-tvals = open('tvals1.dat', 'w')
+fout = File('f2.msh', 'w')
+tvals = open('tvals2.dat', 'w')
 print>>tvals, '#:t Mx Mv Mxx Mvv Mxv W'
 for i in 'Mx:x Mv:v Mxx:x^2 Mvv:v^2 Mxv:xv W:W'.split():
     print>>tvals, r'#:%s.tex=r"\left<%s\right>"'%tuple(i.split(':'))
@@ -30,7 +31,12 @@ M.init(vec(0.,0.), 100000)
 M.f.head = 't=0'; M.f.dump(fout) 
 print>>tvals, M.t, M.av
 
-while M.t<50: # цикл по времени
+chi, t_min, t_max = 0, 10/M.gamma, 10/M.gamma+40*pi/M.Omega
+while M.t<=t_max: # цикл по времени
     M.calc()
     print>>tvals, M.t, M.av
     M.f.head = 't=%g'%M.t; M.f.dump(fout)
+    if M.t>=t_min: chi += M.av[1]*(cos(M.t*M.Omega)+sin(M.t*M.Omega)*1j)*M.h
+
+chi *= 2/(M.A*(t_max-t_min))
+print chi, abs(chi), atan2(chi.imag, chi.real)
