49c4a6d6522bcc4693faf63f2a335aff4cc65c5a
Anton V. Ivanov <aiv.racs@gmail.com>
2020-05-14 21:38:10 +0300

    Исправлена ошибка в /src/view/color.cpp

--- src/view/color.cpp
+++ src/view/color.cpp
@@ -3,7 +3,7 @@
  * Licensed under the Apache License, Version 2.0
  **/
 
-#include "../include/aiwlib/view/color"
+#include "../../include/aiwlib/view/color"
 using namespace aiw;
 
 //------------------------------------------------------------------------------
