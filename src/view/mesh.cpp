9c6691a426596410c82a38902c3d9d480c2ba6f7
Anton V. Ivanov <aiv.racs@gmail.com>
2021-02-06 22:17:16 +0300

    Добавлен менеджер памяти в uplt, теперь можно открывать произвольное число файло

--- src/view/mesh.cpp
+++ src/view/mesh.cpp
@@ -12,13 +12,29 @@
 using namespace aiw;
 // File logfile("uplt.log", "w");
 //------------------------------------------------------------------------------
+std::list<BaseView*> aiw::BaseView::view_table;
+double aiw::BaseView::max_mem_Gsz = 4; // максимально допустимый суммарный размер данных в гигабайтах
+//------------------------------------------------------------------------------
+void aiw::BaseView::mem_load_prepare(){   // расчищает память за счет давно неиспользуемых объектов
+	// printf(">>> %p %i %gG lim %gG\n", this, int(view_table.size()), mem_sz*1e-9, max_mem_Gsz);
+	double tot_mem_Gsz = mem_sz*1e-9;
+	for(auto p: view_table){ if(p==this) return; else tot_mem_Gsz += p->mem_sz*1e-9; }
+	while(!view_table.empty() && tot_mem_Gsz>max_mem_Gsz){
+		// printf("pop %p %gG\n", view_table.back(), view_table.back()->mem_sz*1e-9);
+		tot_mem_Gsz -= view_table.back()->mem_sz*1e-9;
+		view_table.back()->mem_free(); view_table.pop_back();
+	}
+	view_table.push_front(this);
+}
+//------------------------------------------------------------------------------
 bool aiw::MeshView::load(IOstream &S){
 	size_t s = S.tell(); BinaryFormat bf; bf.init(*this); bf.D = -1;
 	if(!bf.load(S) || bf.D&(0xFFFF<<16) || !(bf.D&0xFF)){ S.seek(s); return false; }
 	head = bf.head; D = bf.D; szT = bf.szT; logscale = bf.logscale; 
 	sz = 1; for(int i=0; i<D; i++) sz *= box[i];
 	// WOUT(head, D, szT, sz, logscale, box, bmin, bmax, s, S.tell());
-	mem = S.mmap(sz*szT, 0);
+	// mem = S.mmap(sz*szT, 0);
+	fin = S.copy(); mem_offset = S.tell(); mem_sz = sz*szT; S.seek(mem_sz, 1);  mem.reset();
 	// WOUT(S.tell());
 
 	s = S.tell(); int32_t sz2 = 0; S.load(sz2);  // try read old aivlib mesh format (deprecated)
@@ -36,16 +52,17 @@ bool aiw::MeshView::load(IOstream &S){
 //------------------------------------------------------------------------------
 //bool aiw::MeshView::load_as_sgy(IOstream &S){ if(load(S)){ sgy = true; return true; } else return false; }
 bool aiw::MeshView::load_from_segy(IOstream &S){
+	segy = true; fin = S.copy(); mem_offset = S.tell();
+	mem_load_prepare();  // мы пока не знаем размер данных?
 	Mesh<float, 3> data; Mesh<float, 3> geometry = segy_read(S, data);
 	D = 3; szT = 4; sz = 1; for(int i=0; i<D; i++){ box[i] = data.bbox()[i]; sz *= box[i]; }
+	mem = data.mem; mem_sz = sz*4;
 	bmin[0] = 0; bmax[0] = geometry[ind(6,0,0)]*data.bbox()[0]; this->set_step(0);
 	for(int i=0; i<2; i++){
 		bmin[i+1] = (geometry[ind(i,0,0)]+geometry[ind(3+i,0,0)])/2;
 		bmax[i+1] = (geometry[i|(geometry.bbox()(1,2)-ind(1))]+geometry[(3+i)|(geometry.bbox()(1,2)-ind(1))])/2;
 		this->set_step(i+1);
 	}
-	mem = data.mem;
-	segy = true;
 	return true;
 }
 //------------------------------------------------------------------------------
@@ -95,14 +112,15 @@ aiw::MeshView::access_t::access_t(const MeshView &data, const ConfView &conf){
 	if(conf.segy && !data.segy && conf.axes[1]==0 && conf.get_flip(1)){ powZ2 = true; Z0 = data.box[0]; }
 }
 //------------------------------------------------------------------------------
-std::string aiw::MeshView::get(const ConfView& conf, aiw::Vec<2> r) const {
+std::string aiw::MeshView::get(const ConfView& conf, aiw::Vec<2> r){
+	mem_load();
 	char buf[1024]; double x  = access_t(*this, conf)(r);
 	snprintf(buf, 1023, "%g\n[%i,%i]", x, coord2pos(r[0], conf.axes[0]), coord2pos(r[1], conf.axes[1]));
 	return buf;
 }
 //------------------------------------------------------------------------------
-Vec<2> aiw::MeshView::f_min_max(const ConfView &conf) const { // вычисляет min-max, как это делать для preview?
-	access_t access(*this, conf);
+Vec<2> aiw::MeshView::f_min_max(const ConfView &conf){ // вычисляет min-max, как это делать для preview?
+	mem_load(); access_t access(*this, conf);
 	// WOUT(access.cfa.typeID, access.cfa.offset);
 	float f_min = 0, f_max = 0; int xsz = access.box[0], ysz = access.box[1];
 	if(access.powZ2){
@@ -144,8 +162,8 @@ Vec<2> aiw::MeshView::f_min_max(const ConfView &conf) const { // вычисля�
 	return Vec<2>(f_min, f_max);
 }
 //------------------------------------------------------------------------------
-aiw::Vec<2> aiw::MeshView::f_min_max_tot(const ConfView &conf) const {
-	const char *ptr = (const char*)(mem->get_addr());
+aiw::Vec<2> aiw::MeshView::f_min_max_tot(const ConfView &conf){
+	mem_load();  const char *ptr = (const char*)(mem->get_addr());
 	float f_min = 0, f_max = 0; 
 #pragma omp parallel for reduction(min:f_min) reduction(max:f_max)
 	for(size_t i=0; i<sz; i++){
@@ -156,7 +174,8 @@ aiw::Vec<2> aiw::MeshView::f_min_max_tot(const ConfView &conf) const {
 	return Vec<2>(f_min, f_max);
 }
 //------------------------------------------------------------------------------
-void aiw::MeshView::plot(const ConfView &conf, Image &image, const CalcColor &color) const {
+void aiw::MeshView::plot(const ConfView &conf, Image &image, const CalcColor &color){
+	mem_load();
 	// double t0 = omp_get_wtime();
 	// WOUT(conf.axes);
 	// WOUT(conf.cfa.typeID, conf.cfa.offset, conf.cfa.label, szT);
@@ -220,12 +239,13 @@ auto I = vtx.trace(ind(x, y));
 	// cell_bound ???
 }
 //------------------------------------------------------------------------------
-void aiw::MeshView::preview(const ConfView& conf0, Image &image, const CalcColor &color) const {  // рассчитан на маленькое изображение, учитывает только flip
+void aiw::MeshView::preview(const ConfView& conf0, Image &image, const CalcColor &color){  // рассчитан на маленькое изображение, учитывает только flip
 	ConfView conf = conf0; conf.uncrop();
 	plot(conf, image, color);
 }
 //------------------------------------------------------------------------------
-void aiw::MeshView::get_line(const ConfView &conf, aiw::Vec<2> point, int axe, std::vector<float>& X, std::vector<float>& Y) const {
+void aiw::MeshView::get_line(const ConfView &conf, aiw::Vec<2> point, int axe, std::vector<float>& X, std::vector<float>& Y){
+	mem_load(); 
 	Ind<16> pos = coord2pos(conf.slice); for(int i=0; i<2; i++) pos[conf.axes[i]] = coord2pos(point[i], conf.axes[i]);
 	X.resize(box[axe]); Y.resize(box[axe]);  const char *ptr = (const char*)(mem->get_addr());
 	size_t mul = szT; for(int i=0; i<D; i++){ if(i!=axe){ ptr += mul*pos[i]; } mul *= box[i]; }
