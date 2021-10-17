420e9ac046bc2abc04bdcfc243b75404feb6cb60
Anton V. Ivanov <aiv.racs@gmail.com>
2021-10-17 23:32:56 +0300

    Добавлены палитры qplt и их генератор

---
+++ plot-qplt-pals.cpp
@@ -0,0 +1,12 @@
+#include <sstream>
+#include <aiwlib/qplt/imaging>
+using namespace aiw;
+
+int main(){
+	const char *path = "python3/aiwlib/qplt/pals/";
+	for(auto pal: QpltColor::get_pals()){
+		std::stringstream str; str<<path<<pal<<".ppm";
+		WOUT(pal, str.str());
+		QpltColor::plot_pal(pal.c_str(), 1, 1024, true).dump2ppm(str.str().c_str());
+	}
+}
