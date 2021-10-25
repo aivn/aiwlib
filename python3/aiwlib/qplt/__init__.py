ec23cf9cde8403497ba70cb9d99937f6e157f4f0
Anton V. Ivanov <aiv.racs@gmail.com>
2021-10-25 13:18:20 +0300

    Еще один забытый модуль для qplt

---
+++ python3/aiwlib/qplt/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (C) 2021 Antov V. Ivanov  <aiv.racs@gmail.com> with support Kintech Lab
+# Licensed under the Apache License, Version 2.0
+
+from .factory import *
+from .canvas import *
