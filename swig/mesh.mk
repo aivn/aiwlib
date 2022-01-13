70e14c411b92209091e2fd269dcba4ab6866827f
Anton V. Ivanov <aiv.racs@gmail.com>
2022-01-13 21:50:32 +0300

    Поправлен импорт Mesh в python3

--- swig/mesh.mk
+++ swig/mesh.mk
@@ -18,7 +18,7 @@ $(dst)build/swig/$(MESH_NAME).i: swig/mesh.mk
 	"$(MESH_NAME)_slice$$D(const aiw::Mesh<$(MESH_TYPE), $(MESH_DIM)> &M, aiw::Ind<$(MESH_DIM)> pos, size_t offset){"\
 	"return M.slice<$(MESH_TYPE), $$D>(pos, offset); }%}"; done >> $@
 	@echo '%pythoncode %{$(MESH_NAME).__setstate__ = _setstate %}' >> $@
-	@echo '%pythoncode %{from vec import *%}' >> $@	
+	@echo '%pythoncode %{from .vec import *%}' >> $@	
 	@echo '%pythoncode %{$(MESH_NAME).__C_getitem__, $(MESH_NAME).__C_setitem__ =  $(MESH_NAME).__getitem__, $(MESH_NAME).__setitem__%}' >> $@	
 	@echo '%pythoncode %{$(MESH_NAME).__getitem__ = lambda self, k: self.__C_getitem__(Ind(k) if type(k) in (tuple, list) else k)%}' >> $@	
 	@echo '%pythoncode %{$(MESH_NAME).__setitem__ = lambda self, k, v: self.__C_setitem__(Ind(k) if type(k) in (tuple, list) else k, v)%}' >> $@
