e0d93f5c0bf087b7b224a01ad678740f8d103c23
Anton Ivanov <aiv.racs@gmail.com>
2022-07-15 10:18:34 +0300

    Добавлена функция angle рассчитывающая угол по трем точкам в D-мерном пространстве В qplt в режиме XD на CPU nan сделаны прозрачными

--- src/qplt/mesh_cu.cpp
+++ src/qplt/mesh_cu.cpp
@@ -102,7 +102,8 @@ template <int AID> void aiw::QpltMeshPlotter3D<AID>::plot(int *image) const {
 				// WEXT(pos, pos3d, cID, flat.axis[0], flat.axis[1]);
 				WASSERT(Ind<3>()<= pos3d && pos3d<bbox, "incorrect pos3d", x, y, r, pos, X, pos3d, bbox, cID, ray.fID, ray.gID, ray.f, ray.g, ray.len); 
 				float f = 0; accessor.conv<AID>(ptr, (const char**)nb, &f);
-				if(!D3mingrad || fabs(f0-f)>cr_grad){
+				// if(std::isnan(f)) continue; // прозрачность nan?
+				if(!std::isnan(f) && (!D3mingrad || fabs(f0-f)>cr_grad)){
 					Vecf<4> dC = color(f);
 					float w = /*(1+10*fabs(f0-f)*_df)*/dC[3]*ray.len*_max_len*(1-sum_w);
 					if(sum_w+w<lim_w){ C += dC*w; sum_w += w; }
