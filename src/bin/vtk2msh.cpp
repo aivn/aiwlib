04834d78649a808ea6942980885c1b7cecd24097
Anton V. Ivanov <aiv.racs@gmail.com>
2023-11-05 17:45:06 +0300

    Исправлена ошибка в vtk2msh

--- src/bin/vtk2msh.cpp
+++ src/bin/vtk2msh.cpp
@@ -19,11 +19,12 @@ int main(int argc, const char **argv){
 		std::string src = argv[ipath]; int slash = src.rfind('/'), dot = src.rfind('.');
 		std::string dst = dot>slash? src.substr(0, dot)+".msh": src+".msh"; 
 
-		std::ifstream fin(src); std::ofstream fout(dst);
-
+		std::ifstream fin(src);  if(!fin.good()){ std::cout<<"can't open "<<src<<" --- skipped\n"; continue; }
+		std::ofstream fout(dst); if(!fout.good()){ std::cout<<"can't open "<<dst<<" --- skipped\n"; continue; }
+		
 		aiw::BinaryHead bh; bh.dim = 3; bh.szT = 4;  bh.type = aiw::BinaryHead::mesh;
 		std::vector<float> data; std::string line, word; float step[3];
-		bool read_head_mode = true; size_t counter = 0, frames = 0;
+		bool read_head_mode = true, cell_mode = false; size_t counter = 0, frames = 0;
 
 		do{
 			if(read_head_mode){
@@ -33,17 +34,18 @@ int main(int argc, const char **argv){
 				if(word=="SPACING") for(int i=0; i<3; i++) fin>>step[i];
 				if(word=="ORIGIN") for(int i=0; i<3; i++) fin>>bh.bmin[i];
 				if(word=="POINT_DATA"){ size_t sz;  fin>>sz; data.resize(sz, 0.f); }
+				if(word=="CELL_DATA"){  cell_mode = true; size_t sz;  fin>>sz; data.resize(sz, 0.f); }
 				if(word=="SCALARS") fin>>bh.head;
 				if(word=="default"){
-					counter = 0;
+					counter = 0; for(int i=0; i<3; i++) bh.bbox[i] -= cell_mode;
 					for(int i=0; i<3; i++) if(bh.bbox[i]==1){
 							bh.dim = 2;
 							for(int j=i+1; j<3; j++){ bh.bbox[j-1] = bh.bbox[j]; bh.bmin[j-1] = bh.bmin[j]; step[j-1] = step[j]; }
 							break;
 						}
 					for(int i=0; i<3; i++) bh.bmax[i] = bh.bmin[i] + step[i]*bh.bbox[i];
-					read_head_mode = false;
-				} 
+					read_head_mode = cell_mode = false;
+				}
 				continue;
 			}
 			fin>>data[counter++];
